ba567e8a16652f9657b5dc4455191b2a200dc4c7
asifshaikh7866 <160568104+asifshaikh7866@users.noreply.github.com>
2024-05-29 16:50:23 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
-var students = [
-    { name: "Anwar", isSenior: true, hasCompletedAssignments: true },
-    { name: "Maroof", isSenior: false, hasCompletedAssignments: false },
-    { name: "Faisal", isSenior: true, hasCompletedAssignments: false },
-    { name: "Zaheer", isSenior: false, hasCompletedAssignments: true },
-    { name: "Jamal", isSenior: true, hasCompletedAssignments: true },
-];
-function findSeniorStudentsWithAssignments(students) {
-    return students.filter(function (student) { return student.isSenior && student.hasCompletedAssignments; });
-}
-var seniorStudentsWithAssignments = findSeniorStudentsWithAssignments(students);
-console.log("Senior Students with Assignments :", seniorStudentsWithAssignments);
-function removeIncompleteAssignments(students) {
-    return students.filter(function (student) { return student.isSenior && student.hasCompletedAssignments; });
-}
-var updatedClassList = removeIncompleteAssignments(students);
-console.log("Updated Class List :", updatedClassList);
+var inventory = [];
+var product1 = {
+    name: "Honda CD125",
+    model: "2023",
+    cost: 240990,
+    quantity: 50,
+};
+var product2 = {
+    name: "Printer",
+    model: "2022",
+    cost: 300000,
+    quantity: 800,
+};
+var product3 = {
+    name: "Honda Civic",
+    model: "2024",
+    cost: 4500000,
+    quantity: 15
+};
+inventory = [product1, product2, product3];
+console.log(inventory);
+var accessThirdProperty = [inventory[2].quantity];
+console.log(accessThirdProperty);
+var newProduct = {
+    name: "Laptop",
+    model: "New",
+    cost: 65000,
+    quantity: 70,
+};
+;
+var updatedInventory = [product1, product2, product3, newProduct];
+console.log(updatedInventory);
+var accessSecondProperty = [updatedInventory[1].name];
+console.log(accessSecondProperty);
